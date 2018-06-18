e09fcba9ca9fb7f292c2bf6091e86f57fb82e400
lameme <esme.riveroh@gmail.com>
2018-06-17 23:03:10 -0500

    Readme y documentación

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,7 @@
 window.cipher = {
   offset: 33,
   string: "",
+  //entra a la función del cifrado
   encode: (offset, string) => {
     let arrayTextToCipher = [];
     let arrayTextCiphered = [];
@@ -12,21 +13,26 @@ window.cipher = {
           arrayTextToCipher.push(32);
           textCiphered += String.fromCharCode(string.charCodeAt([i]));
         }
-
+        /*Se validan las letras minúsculas y se agregan al arreglo arrayTextToCipher las letras en código ascii
+        arrayTextCiphered contendrá el nuevo número ya cifrado con la fórmula
+        textCiphered almacenará la cadena convertida de un número (arrayTextCiphered) a letra
+        */
         else if (string.charCodeAt([i]) > 96 && string.charCodeAt([i]) < 123) {
           arrayTextToCipher.push(string.charCodeAt([i]));
           arrayTextCiphered = ((((arrayTextToCipher[i] - 97 ) + parseInt(offset)) % 26)  + 97);
           textCiphered += String.fromCharCode(arrayTextCiphered);
         }
-        //en caso contrario se realiza la codificación
+        /*Se validan las letras minúsculas y se agregan al arreglo arrayTextToCipher las letras en código ascii
+        arrayTextCiphered contendrá el nuevo número ya cifrado con la fórmula
+        textCiphered almacenará la cadena convertida de un número (arrayTextCiphered) a letra
+        */
         else {
           arrayTextToCipher.push(string.charCodeAt([i]));
           arrayTextCiphered = ((((arrayTextToCipher[i] - 65 ) + parseInt(offset)) % 26)  + 65);
           textCiphered += String.fromCharCode(arrayTextCiphered);
         }
         }
-      //se muestra el texto cifrado en el input correspondiente
-        // outputTextC.defaultValue = textCiphered;
+        //se regresa la cadena para que pueda ser usada en index.js
         return textCiphered;
   },
   decode: (offset, string) => {
@@ -40,20 +46,27 @@ window.cipher = {
           arrayTextToDecipher.push(32);
           textDeciphered += String.fromCharCode(string.charCodeAt([i]));
         }
-
+        /*Se validan las letras minúsculas y se agregan al arreglo arrayTextToDecipher las letras en código ascii
+        arrayTextDeciphered contendrá el nuevo número ya cifrado con la fórmula
+        textDeciphered almacenará la cadena convertida de un número (arrayTextDeciphered) a letra
+        */
         else if (string.charCodeAt([i]) >= 97 && string.charCodeAt([i]) <= 122) {
           arrayTextToDecipher.push(string.charCodeAt([i]));
           arrayTextDeciphered = ((((arrayTextToDecipher[i] + 111 ) - parseInt(offset)) % 26)  + 97);
           textDeciphered += String.fromCharCode(arrayTextDeciphered);
         }
-        //en caso contrario se realiza la codificación
+        /*Se validan las letras minúsculas y se agregan al arreglo arrayTextToDecipher las letras en código ascii
+        arrayTextDeciphered contendrá el nuevo número ya cifrado con la fórmula
+        textDeciphered almacenará la cadena convertida de un número (arrayTextDeciphered) a letra
+        */
         else {
           arrayTextToDecipher.push(string.charCodeAt([i]));
           arrayTextDeciphered = ((((arrayTextToDecipher[i] + 65 ) - parseInt(offset)) % 26)  + 65);
           textDeciphered += String.fromCharCode(arrayTextDeciphered);
         }
       }
-        return textDeciphered;
+      //se regresa la cadena para que pueda ser usada en index.js
+      return textDeciphered;
   },
 
   createCipherWithOffset: (offset, string) => {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//Creación de las variables a usar
 let offset = document.getElementById("textValueBean");
 let string = document.getElementById("textCipher");
 let botonCifrar = document.getElementById("cifrar");
@@ -6,21 +7,24 @@ let outputTextC = document.getElementById("newTextCiphered");
 let outputTextD = document.getElementById("newTextDeciphered");
 let stringD = document.getElementById("newTextCiphered");
 
+//Llama al objeto window cipher encode y envía los valores de offset y string
 botonCifrar.addEventListener("click", event => {
   let encodeValueBean = window.cipher.encode(
     offset.value,
     string.value
   );
+  //Muestra el retorno de cipher encode
   console.log(encodeValueBean);
   outputTextC.defaultValue = encodeValueBean;
-
 });
 
+//Llama al objeto window cipher decode y envía los valores de offset y string
 botonDescifrar.addEventListener("click", event => {
   let decodeValueBean = window.cipher.decode(
     offset.value,
     stringD.value
   );
+  //Muestra el retorno de cipher decode
   console.log(decodeValueBean);
   outputTextD.defaultValue = decodeValueBean;
 });
